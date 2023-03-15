f50d9a44f0091d01cda50be1f8d9c5c0d1be0276
A7medabdelaty <elqahwagy.123@gmail.com>
2023-03-15 19:20:10 +0200

    create font manager & app class

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManger {
+  static const primary = Color(0xffED9728);
+  static const darkGrey = Color(0xff525252);
+  static const lightGrey = Color(0xff9E9E9E);
+  static const grey = Color(0xff737477);
+}
